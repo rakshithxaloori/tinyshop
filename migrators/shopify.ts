879db53e935d0b4032c425ef96544682e366b106
Koustav Chowdhury <kc99.kol@gmail.com>
2024-07-13 19:08:02 +0530

    v0.0.17 released. migrator script updated to enter collections too

--- migrators/shopify.ts
+++ migrators/shopify.ts
@@ -22,7 +22,7 @@ const convertToProductCreate = (product: any): ProductCreate => {
   return {
     name: product.title,
     description: product.body_html,
-    images: product.images.map((image: any) => image.src),
+    images: product.images.map((image: any) => image.src) || [],
     active: product.status === "active",
     shippable: product.variants.some(
       (variant: any) => variant.requires_shipping
@@ -95,43 +95,70 @@ const convertToPriceCreate = (
   };
 };
 
+const collectionImagesList = [
+  "https://images.unsplash.com/photo-1718615111834-c44e5bfbaca3?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1695221087406-257eca10a2e7?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1569541838345-b92c004f9046?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1669749614397-2b9f87ab2168?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+]
+
 // Main function to process JSON data
 const processJSONData = async (filePath: string) => {
   const data = readJSON(filePath);
   const products = data.products;
 
-  products.forEach(async (product: any) => {
-    if (product.status !== "active") return;
+  const activeProducts = products.filter(
+    (product: any) => {
+      const isActive = product.status === "active";
+      const hasImage = product.images.length > 0;
+      return isActive && hasImage;
+    }
+  );
+
+  let shopifyIdToTinyshopProductId: any = {};
+
+  for (const product of activeProducts) {
     const productCreate = convertToProductCreate(product);
-    // productCreates.push(productCreate);
     const product_res = await tinyshop.products.create(productCreate);
-    console.log(product_res);
+    shopifyIdToTinyshopProductId[product.id] = product_res.id;
 
-    product.options.forEach(async (option: any) => {
+    for (const option of product.options) {
       const optionCreate = convertToOptionCreate(option, product_res.id);
-      // optionCreates.push(optionCreate);
-      const option_res = await tinyshop.options.create(optionCreate);
-      console.log(option_res);
-    });
+      await tinyshop.options.create(optionCreate);
+    }
 
-    product.variants.forEach(async (variant: any, index) => {
+    for (const [index, variant] of product.variants.entries()) {
       const variantCreate = convertToVariantCreate(variant, product_res.id);
       if (index === 0) {
         variantCreate.is_default = true;
       }
-
-      // variantCreates.push(variantCreate);
       const variant_res = await tinyshop.variants.create(variantCreate);
-      console.log(variant_res);
-
       const priceCreate = convertToPriceCreate(variant, variant_res.id);
-      // priceCreates.push(priceCreate);
-      const price_res = await tinyshop.prices.create(priceCreate);
-      console.log(price_res);
-    });
-  });
+      await tinyshop.prices.create(priceCreate);
+    }
+  }
+
+  const collections = data.custom_collections;
+  console.log("Done creating products, options, variants, and prices. Now creating collections.")
+
+  for (const collection of collections) {
+
+    const products = collection.products.map((productId: any) => shopifyIdToTinyshopProductId[productId]);
+
+    const collectionCreate = {
+      name: collection.title,
+      image_web: collectionImagesList[Math.floor(Math.random() * collectionImagesList.length)],
+      image_mobile: collectionImagesList[Math.floor(Math.random() * collectionImagesList.length)],
+      products: products.filter((product: any) => product !== undefined),
+    };
+    const collection_res = await tinyshop.collections.create(collectionCreate);
+  }
+
+
+  console.log("Done creating collections.");
 };
 
 // Run the main function with the provided JSON file
 console.log(__dirname);
-processJSONData(path.resolve(__dirname, "store_data/wellnesslanguage.json"));
+// processJSONData(path.resolve(__dirname, "store_data/wellnesslanguage.json"));
+processJSONData(path.resolve(__dirname, "store_data/getabranddeal.json"));
--- src/api.ts
+++ src/api.ts
@@ -39,14 +39,14 @@ class ApiService {
     endpoint: string,
     queryParams?: any
   ): Promise<// | Product
-  // | ProductList
-  // | Option
-  // | OptionList
-  // | Variant
-  // | VariantList
-  // | Price
-  // | PriceList
-  any> {
+    // | ProductList
+    // | Option
+    // | OptionList
+    // | Variant
+    // | VariantList
+    // | Price
+    // | PriceList
+    any> {
     if (queryParams) {
       const query = urlEncode(queryParams);
       endpoint = `${endpoint}?${query}`;
@@ -61,16 +61,16 @@ class ApiService {
   async post(
     endpoint: string,
     data: //   | ProductCreate
-    //   | ProductUpdate
-    //   | OptionCreate
-    //   | OptionUpdate
-    //   | VariantCreate
-    //   | VariantUpdate
-    //   | PriceCreate
-    //   | PriceUpdate
-    any
+      //   | ProductUpdate
+      //   | OptionCreate
+      //   | OptionUpdate
+      //   | VariantCreate
+      //   | VariantUpdate
+      //   | PriceCreate
+      //   | PriceUpdate
+      any
   ): Promise<//   Product | Option | Variant | Price
-  any> {
+    any> {
     const url = `${this.baseUrl}${endpoint}`;
     const formBody = urlEncode(data);
     const response = await apiFetch(url, this.secretKey, {
@@ -86,7 +86,7 @@ class ApiService {
   async delete(
     endpoint: string
   ): Promise<//   ProductDelete | OptionDelete | VariantDelete | PriceDelete
-  any> {
+    any> {
     const url = `${this.baseUrl}${endpoint}`;
     const response = await apiFetch(url, this.secretKey, {
       method: "DELETE",
