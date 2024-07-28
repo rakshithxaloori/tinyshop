885763c845025da4bec6d5903307c9c36c0d63f1
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2024-07-28 09:58:11 +0530

    max overflow -1

--- database.py
+++ database.py
@@ -6,4 +6,6 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_size=20, max_overflow=0)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, echo=True, pool_size=20, max_overflow=-1
+)
