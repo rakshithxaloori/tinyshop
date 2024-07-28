ab5711eedce2fdaac1222418860ae390b03e281b
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2024-07-28 09:57:16 +0530

    increase pool size

--- database.py
+++ database.py
@@ -6,4 +6,4 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_size=20, max_overflow=0)
