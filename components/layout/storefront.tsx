7abbaa18aee7d1ca2588c9a38fb033f2e2f497ef
Koustav Chowdhury <kc99.kol@gmail.com>
2024-07-31 17:22:26 +0530

    Fix view on mobile

--- components/layout/storefront.tsx
+++ components/layout/storefront.tsx
@@ -18,7 +18,7 @@ const StorefrontLayout = async ({
       {...(theme ? { 'data-theme': theme } : {})}
     >
       <BasicHeader {...{ navItems }} />
-      <div className="flex flex-col flex-1 max-w-7xl self-center">
+      <div className="flex flex-col flex-1 md:max-w-7xl md:self-center">
         {children}
       </div>
       <BasicFooter />
