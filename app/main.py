ea768581aa96529802dc2479ac7442cc9e426775
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2024-08-02 20:20:54 +0530

    test endpoint forward

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+import os
 import base64
+import requests
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+TEST_ENDPOINT = os.environ["TEST_ENDPOINT"]
 
 
 @app.middleware("http")
@@ -16,8 +20,37 @@ async def get_credentials(request: Request, call_next):
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
         )
 
-    username, _ = base64.b64decode(data).decode().split(":", 1)
-    key_type, mode, _ = username.split("_")
+    try:
+        username, _ = base64.b64decode(data).decode().split(":", 1)
+        key_type, mode, _ = username.split("_")
+    except (ValueError, base64.binascii.Error):
+        return JSONResponse(
+            content={"message": "Invalid authorization format"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if mode == "test":
+        new_url = f"{TEST_ENDPOINT.rstrip('/')}{request.url.path}"
+
+        # Extract headers and remove 'host' as it's set by requests automatically
+        headers = {k: v for k, v in request.headers.items() if k.lower() != "host"}
+
+        # Make the request to the TEST_ENDPOINT
+        response = requests.request(
+            method=request.method,
+            url=new_url,
+            headers=headers,
+            data=await request.body(),
+            params=request.query_params,
+        )
+
+        # Return the response from the TEST_ENDPOINT
+        return Response(
+            content=response.content,
+            status_code=response.status_code,
+            headers=dict(response.headers),
+        )
+
     return JSONResponse(
         content={"message": f"Type: {key_type}; Mode: {mode}"},
         status_code=status.HTTP_200_OK,
@@ -33,8 +66,11 @@ def read_root():
 
 # docker run -d --name ts-proxy-container -p 80:80 ts-proxy
 
-# curl http://127.0.0.1/v1/products \
-#   -u sk_test_tR3PYbcVNZZ796tH88S4VQ2u:
+# curl http://127.0.0.1:8081/v1/products \
+#   -u sk_test_aunh1MXIU0sUX2nzIcbtMqRR:
 
 # curl http://ts-dev-lb-399352269.us-east-1.elb.amazonaws.com   \
+#   -u sk_test_tR3PYbcVNZZ796tH88S4VQ2u:
+
+# curl http://internal-ts-dev-api-lb-1872903523.us-east-1.elb.amazonaws.com/v1/products   \
 #   -u sk_test_tR3PYbcVNZZ796tH88S4VQ2u:
--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-docker build -t ts-proxy-api .
+# docker build --no-cache -t ts-proxy-api .
+export $(grep -v '^#' .env | xargs) && \
+docker build \
+-t ts-proxy-api . \
+--no-cache \
+--build-arg TEST_ENDPOINT=$TEST_ENDPOINT \
