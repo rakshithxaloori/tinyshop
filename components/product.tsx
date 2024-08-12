e733ccee91fe19ddc43e5c67bbbd09fbc3eae95b
Koustav Chowdhury <kc99.kol@gmail.com>
2024-08-12 15:46:50 +0530

    Basic working POC

--- components/product.tsx
+++ components/product.tsx
@@ -47,33 +47,30 @@ const ProductCard: React.FC<ProductCardProps> = ({ data, layout }) => {
   console.log(layout);
 
   return (
-    <div className={cn("h-[20vh] w-[50vw]", layout.containerClass)}>
-
-    </div>
-    // <Card className={cn(`w-full h-[20vh] max-w-sm mx-auto bg-blue-100`, layout.containerClass)}>
-    //   <div className={cn("relative h-24 w-24", layout.imageClass)}>
-    //     <Image
-    //       src={images[0] || '/api/placeholder/400/400'}
-    //       alt={name}
-    //       width={96}
-    //       height={96}
-    //       className="aspect-square rounded-lg"
-    //     />
-    //   </div>
-    //   <div className={layout.contentClass}>
-    //     <h2 className={layout.titleClass}>{name}</h2>
-    //     <p className={layout.variantClass}>{default_variant.name}</p>
-    //     <div className={layout.priceClass}>
-    //       <span>{formatPrice(price.unit_amount)}</span>
-    //       {price.unit_compare_amount && (
-    //         <span className={layout.compareAtPriceClass}>
-    //           {formatPrice(price.unit_compare_amount)}
-    //         </span>
-    //       )}
-    //     </div>
-    //     <Badge className={layout.badgeClass} variant="secondary">{price.type}</Badge>
-    //   </div>
-    // </Card>
+    <Card className={cn(`w-full h-fit max-w-sm mx-auto bg-blue-100`, layout.containerClass)}>
+      <div className={cn("relative h-24 w-24", layout.imageClass)}>
+        <Image
+          src={images[0] || '/api/placeholder/400/400'}
+          alt={name}
+          width={96}
+          height={96}
+          className="aspect-square rounded-lg"
+        />
+      </div>
+      <div className={layout.contentClass}>
+        <h2 className={layout.titleClass}>{name}</h2>
+        <p className={layout.variantClass}>{default_variant.name}</p>
+        <div className={layout.priceClass}>
+          <span>{formatPrice(price.unit_amount)}</span>
+          {price.unit_compare_amount && (
+            <span className={layout.compareAtPriceClass}>
+              {formatPrice(price.unit_compare_amount)}
+            </span>
+          )}
+        </div>
+        <Badge className={layout.badgeClass} variant="secondary">{price.type}</Badge>
+      </div>
+    </Card>
   );
 }
 
