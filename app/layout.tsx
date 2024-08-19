00246b7743ada17e792b4d08e461ace9bc2756d8
Koustav Chowdhury <kc99.kol@gmail.com>
2024-08-19 19:54:37 +0530

    Remove dep on NEXT_PUBLIC_SHOP_NAME

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,24 +5,32 @@ import "./globals.css";
 import Transitions, { Animate } from "@/components/transitions";
 import { Toaster } from "@/components/ui/sonner";
 import ClientSideProvider from "./providers";
+import { getProductLayoutDetails } from "@/lib/storefront";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const shopName = process.env.NEXT_PUBLIC_SHOP_NAME || "tinyshop";
-const globalTheme = process.env.NEXT_PUBLIC_THEME || "retro";
+// const globalTheme = process.env.NEXT_PUBLIC_THEME || "retro";
 
 export const metadata: Metadata = {
   title: `${shopName}`,
   description: "e-commerce site powered by tinyshop",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const mongoLayout = await getProductLayoutDetails(shopName);
+  const cardLayout = mongoLayout?.layout || null;
+  if (!cardLayout) {
+    throw new Error('No layout found for the brand');
+  }
+  const theme = mongoLayout?.theme || "retro";
+
   return (
-    <html lang="en" className="" data-theme={globalTheme}>
+    <html lang="en" className="" data-theme={theme}>
       <body className={inter.className}>
         <Transitions>
           <Animate>
